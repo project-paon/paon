dc4e4abfdb2c11df85be38c1c44c2152417aa460
AbyssTechnique <sp.eurocircle@gmail.com>
2016-02-18 11:39:10 +0100

    tests envoi json

--- Back/register.php
+++ Back/register.php
@@ -1,34 +1,39 @@
 <?php
-  include("connectionBDD.php");
 
-  $pseudo = $_POST['pseudo'];
-  $firstname = $_POST['firstname'];
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $image = $_POST['img'];
+  $test = $_POST['userJson'];
+  echo $test;
 
 
-    try{
+  //include("connectionBDD.php");
 
-        $testPseudo = $bdd->query("SELECT * FROM users WHERE pseudo = '$pseudo'");
-    }catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
+//  $pseudo = $_POST['pseudo'];
+//  $firstname = $_POST['firstname'];
+//  $name = $_POST['name'];
+//  $email = $_POST['email'];
+//  $password = $_POST['password'];
+//  $image = $_POST['img'];
 
-  if ($testPseudo->rowCount() > 0){
-        header('HTTP/1.1 422 pseudo already taken');
-        echo json_encode('{"statut":"false","erreur" : "'.$pseudo.' déjà utilisé"}');
-  }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    header('HTTP/1.1 422 invalid email');
-  }elseif(strlen($password) < 8){
-      header('HTTP/1.1 422 to short password');
-  }else {
-    $bdd->query("INSERT INTO users VALUES($pseudo, $name,$firstname,$email,$password,$image)");
-    header('HTTP/1.1 201 OK');
-    echo json_encode('{"statut":"true"}');
-  }
 
+  //   try{
+  //
+  //       $testPseudo = $bdd->query("SELECT * FROM users WHERE pseudo = '$pseudo'");
+  //   }catch(Exception $e)
+  //   {
+  //       die('Erreur : '.$e->getMessage());
+  //   }
+  //
+  // if ($testPseudo->rowCount() > 0){
+  //       header('HTTP/1.1 422 pseudo already taken');
+  //       echo json_encode('{"statut":"false","erreur" : "'.$pseudo.' déjà utilisé"}');
+  // }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+  //   header('HTTP/1.1 422 invalid email');
+  // }elseif(strlen($password) < 8){
+  //     header('HTTP/1.1 422 to short password');
+  // }else {
+  //   $bdd->query("INSERT INTO users VALUES($pseudo, $name,$firstname,$email,$password,$image)");
+  //   header('HTTP/1.1 201 OK');
+  //   echo json_encode('{"statut":"true"}');
+  // }
+  //
 
 ?>
