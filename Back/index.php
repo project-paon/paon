537d14cea510cb76c03de0bc7fc937d010d0eaa7
Karine.J <karine.jamet203@gmail.com>
2016-02-19 14:39:47 +0100

    BACK : autorisation de connexion dans index.php

--- Back/index.php
+++ Back/index.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type');
+
 
 $uri = $_SERVER['REQUEST_URI'];
 
