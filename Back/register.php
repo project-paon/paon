ca33337a466a2d9d38a30b8632b5c59e8f368c51
Karine.J <karine.jamet203@gmail.com>
2016-02-19 15:45:57 +0100

    BACK : creation de session a l'inscription et gestion d'erreur

--- Back/register.php
+++ Back/register.php
@@ -21,18 +21,64 @@ include('connectionBDD.php');
 
   if ($testPseudo->rowCount() > 0){
         header('HTTP/1.1 422 pseudo already taken');
-        echo ('{"statut":"false","erreur" : "'.$pseudo.' déjà utilisé"}');
+        echo ('{"statut":"false","erreur" : "'.$pseudo.' déjà utilisé"}, "type":"1"');
   }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo ('{"statut":"false","erreur" : "Adresse email invalid."}, "type":"2"');
     header('HTTP/1.1 422 invalid email');
+
   }elseif(strlen($password) < 8){
+        echo ('{"statut":"false","erreur" : "Mot de passe trop court."}, "type":"3"');
       header('HTTP/1.1 422 to short password');
   }else {
     $bdd->query("INSERT INTO users VALUES($pseudo, $name,$firstname,$email,$password,$image)");
     header('HTTP/1.1 201 OK');
-    echo ('{"statut":"true"}');
+    $session = generateUniqueId(15) ;
+    echo ('{"statut":"true","session":"'.$session.'"}');
   }
 }
 else {
   header('HTTP/1.1 400 no method');
 }
+
+
+
+function generateUniqueId($maxLength = null) {
+    $entropy = '';
+
+    // try ssl first
+    if (function_exists('openssl_random_pseudo_bytes')) {
+        $entropy = openssl_random_pseudo_bytes(64, $strong);
+        // skip ssl since it wasn't using the strong algo
+        if($strong !== true) {
+            $entropy = '';
+        }
+    }
+
+    // add some basic mt_rand/uniqid combo
+    $entropy .= uniqid(mt_rand(), true);
+
+    // try to read from the windows RNG
+    if (class_exists('COM')) {
+        try {
+            $com = new COM('CAPICOM.Utilities.1');
+            $entropy .= base64_decode($com->GetRandom(64, 0));
+        } catch (Exception $ex) {
+        }
+    }
+
+    // try to read from the unix RNG
+    if (is_readable('/dev/urandom')) {
+        $h = fopen('/dev/urandom', 'rb');
+        $entropy .= fread($h, 64);
+        fclose($h);
+    }
+
+    $hash = hash('whirlpool', $entropy);
+    if ($maxLength) {
+        return substr($hash, 0, $maxLength);
+    }
+    return $hash;
+}
+
+
 ?>
