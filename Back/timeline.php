64bc681f0ca6f174a4100562ac104b8cabe65afa
Karine SIMPLon MARS 2 <karine.jamet203@gmail.com>
2016-02-22 10:17:22 +0100

    BACK : Correction conflit avec front

--- Back/timeline.php
+++ Back/timeline.php
@@ -1,20 +1,6 @@
 <?php
 include("connectionBDD.php");
 
-  try{
-
-     $tweets = $bdd->query('SELECT * FROM tweets');
-  }
-  catch(Exception $e)
-  {
-      header('HTTP/1.1 400 crash BDD');
-      die('Erreur : '.$e->getMessage());
-  }
-  
-  $resultat = $tweets->fetchAll();
-  
-  echo json_encode($resultat); // Remplacer echo par la réponse
-
 $pseudo = $_POST['pseudo'];
 $session = $_POST['session'];
 
